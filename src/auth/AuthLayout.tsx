2854d85354432af0231fa706291c67e6a14fe61d
Olivier PERDRIX <olivierperdrix@live.fr>
2024-10-09 12:55:09 +0200

    * chore(AuthLayout.tsx): update import path for AuthHeader component

--- src/auth/AuthLayout.tsx
+++ src/auth/AuthLayout.tsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import AuthHeader from "../components/auth/AuthHeader";
+import AuthHeader from "../components/nav/AuthHeader";
 
 export default function Layout() {
   return (

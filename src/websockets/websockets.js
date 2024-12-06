e3c68b8c59c30ff4445bf8215a080a7bec4a7a23
Olivier PERDRIX <olivierperdrix@live.fr>
2024-12-06 15:36:12 +0100

    fix socket communication

--- src/websockets/websockets.js
+++ src/websockets/websockets.js
@@ -1,30 +1,43 @@
 import { getGame, updateGame } from "../controllers/games.js";
 import { joinRoom } from "./room/joinRoom.js";
+
 export function websockets(app) {
+  app.ready().then(() => {
     app.io.on("connection", (socket) => {
-        console.log(`Joueur connecté : ${socket.id}`);
+      console.log(`Joueur connecté : ${socket.id}`);
 
-        joinRoom(socket);
+      joinRoom(socket);
 
-        playerJoinedGame(socket);
+      playerJoinedGame(socket, app.io);
 
-        socket.on("disconnect", () => {
-            console.log(`Joueur déconnecté : ${socket.id}`);
-        });
+      socket.on("disconnect", () => {
+        console.log(`Joueur déconnecté : ${socket.id}`);
+      });
     });
+  });
 }
 
-function playerJoinedGame(socket) {
-    socket.on("player-joined-game", async ({ room, userId }) => {
-        console.log("player-joined-game", room, userId);
-        updateGame({ params: { action: "join", gameId: room }, body: { userId } });
-        const game = await getGame(room);
-        if (!game) {
-            return;
-        }
-        socket.join(room);
-        setTimeout(() => {
-            socket.to(room).emit("player-joined-game", game);
-        }, 1000);
-    });
+// un joueur rejoint une partie
+// on envoie à tous les joueurs de la partie les informations de la partie
+function playerJoinedGame(socket, io) {
+
+  socket.on("player-joined-game", async ({ room, userId }) => {
+    console.log("player-joined-game", room, userId);
+
+    updateGame({ params: { action: "join", gameId: room }, body: { userId } });
+
+    const game = await getGame(room);
+    if (!game) {
+      console.error("Game not found for room:", room);
+      return;
+    }
+
+    // Ajouter le socket à la room
+    socket.join(room);
+
+    // Émettre l'événement à tous les membres de la room
+    io.to(room).emit("player-joined-game", game);
+  });
+
 }
+

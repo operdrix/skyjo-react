e565bda02601290627e00ebff12a064f474324bc
Olivier PERDRIX <olivierperdrix@live.fr>
2025-01-28 21:07:15 +0100

    * chore(Game.tsx): add left buttons section * feat(Game.tsx): add rules button with link to rules video * chore(Game.tsx): add right buttons section

--- frontend/src/pages/game/Game.tsx
+++ frontend/src/pages/game/Game.tsx
@@ -235,6 +235,7 @@ const Game = () => {
         ${game.players.length === 2 ? 'grid-cols-1' : game.players.length === 3 ? 'grid-cols-2' : 'grid-cols-3'}
         `}>
 
+        {/* Boutons de gauche */}
         <div className="flex flex-col gap-4 absolute p-2 mt-10">
           <div className="tooltip tooltip-right" data-tip="Retour à l'accueil">
             <button
@@ -274,13 +275,17 @@ const Game = () => {
             </button>
           </div>
           {/* Bouton pour afficher les règles */}
-          {/* <div className="tooltip tooltip-right" data-tip="Règles du jeu">
-            <button className="btn btn-circle">
+          <div className="tooltip tooltip-right" data-tip="Règles du jeu">
+            <a className="btn btn-circle" href="https://www.youtube.com/watch?v=4BJv_vDqou8" target="_blank" rel="noreferrer">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
               </svg>
-            </button>
-          </div> */}
+            </a>
+          </div>
+        </div>
+
+        {/* Boutons de droite */}
+        <div className="flex flex-col gap-4 absolute p-2 mt-10 right-0">
           <ToggleTheme className="btn-circle" />
           <label className="swap swap-rotate btn btn-circle">
             <input type="checkbox" checked={sound} onChange={() => setSound(!sound)} />
